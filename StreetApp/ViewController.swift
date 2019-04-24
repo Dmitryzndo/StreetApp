b9485a63d40f74de140a528b30b383f74202e984
dimkasp@mail.ru <dimkasp@mail.ru>
2019-04-24 15:32:38 +0300

    Initial Commit

---
+++ StreetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StreetApp
+//
+//  Created by Dmitry Belousov on 24/04/2019.
+//  Copyright © 2019 Dmitry Belousov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
